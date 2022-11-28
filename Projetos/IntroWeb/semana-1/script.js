8c3bf89d9f630cbb5412f036022ca6fd866541c2
nlperri <lnataliaperri@gmail.com>
2022-11-28 10:59:45 -0300

    exercises update

--- Projetos/IntroWeb/semana-1/script.js
+++ Projetos/IntroWeb/semana-1/script.js
@@ -1,18 +1,18 @@
-const music1 = "CARDIGAN";
+const music1 = "Cardigan";
 const artist1 = "Taylor Swift";
 const album1 = "Folklore";
 const dur1 = 3.59;
 const genre1 = "Folk-pop";
 const save1 = false;
 
-const music2 = "SCOTT STREET";
+const music2 = "Scott Street";
 const artist2 = "Phoebe Bridgers";
 const album2 = "Stranger in the Alps";
 const dur2 = 5.05;
 const genre2 = "Indie-folk";
 const save2 = true;
 
-const music3 = "EVERYTHING IN ITS RIGHT PLACE";
+const music3 = "Everything in Its Right Place";
 const artist3 = "Radiohead";
 const album3 = "Kid A";
 const dur3 = 4.11;
@@ -38,11 +38,11 @@ function data(music, artist, album, dur, genre, save) {
   return info;
 }
 
-console.table(data(music1, artist1, album1, dur1, genre1, save1));
+console.table(data(music1.toUpperCase(), artist1, album1, dur1, genre1, save1));
 
-console.table(data(music2, artist2, album2, dur2, genre2, save2));
+console.table(data(music2.toUpperCase(), artist2, album2, dur2, genre2, save2));
 
-console.table(data(music3, artist3, album3, dur3, genre3, save3));
+console.table(data(music3.toUpperCase(), artist3, album3, dur3, genre3, save3));
 
 console.log(`\n-----This is your top favorite artists from last month:-----\n`);
 console.table(favArtists);
